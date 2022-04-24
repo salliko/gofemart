bf934658e0bb9d365133859088c56b9b2f3edf71
grebennikov_iy <grebennikov_iy@magnit.ru>
2022-04-24 14:37:10 +0300

    fix: добавил хедер json при 401 ответе

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -9,6 +9,8 @@ func CheckCookie(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if _, err := r.Cookie("user_id"); err != nil {
 			log.Print("пользователь не аутентифицирован")
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(http.StatusUnauthorized)
 			http.Error(w, "пользователь не аутентифицирован", http.StatusUnauthorized)
 			return
 		}
